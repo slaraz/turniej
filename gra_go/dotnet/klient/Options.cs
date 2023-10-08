2c147a42c3fb0b173c7b63c8c09ed866b8399a6e
Maciej Koprowski <maciej.koprowski@r.pl>
2023-10-09 00:26:36 +0200

    Pierwsza naiwna implementacja klienta .net na podstawie klienta go

---
+++ gra_go/dotnet/klient/Options.cs
@@ -0,0 +1,17 @@
+﻿using CommandLine.Text;
+using CommandLine;
+
+public class Options
+{
+    [Option("nazwa", Required = false, HelpText = "nazwa gracza")]
+    public string Nazwa { get; set; } = "Ziutek .Net";
+
+    [Option('a', "addr", Required = false, HelpText = "wskazuje nazwę serwera do podłączenia")]
+    public string Serwer { get; set; } = "http://localhost:50051";
+
+    [Option('n', "nowa", Required = true, HelpText = "rozpoczyna nową grę", SetName = "tryb1")]
+    public bool CzyNowa { get; set; }
+    
+    [Option('g', "gra", Required = true, HelpText = "dołącza do gry o podanej nazwie", SetName = "tryb2")]
+    public string GraId { get; set; }
+}
